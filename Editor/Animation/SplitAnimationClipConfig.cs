d1fb63546a5c4cee533d99f94b065ebbcecdf3d9
BanMing <ban-ming@foxmail.com>
2020-11-20 08:47:27 +0800

    add split animation editor window

---
+++ Editor/Animation/SplitAnimationClipConfig.cs
@@ -0,0 +1,45 @@
+﻿/******************************************************************
+** @File         : SplitAnimationClipConfig.cs
+** @Author       : BanMing 
+** @Date         : 11/20/2020 8:14:36 AM
+** @Description  :  
+*******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace BMBaseCore
+{
+    //[CreateAssetMenu(fileName = "SplitAnimationClipConfig")]
+    [Serializable]
+    public class SplitAnimationClipConfig : ScriptableObject
+    {
+
+        private const string PATH = @"Assets\Scripts\BMBaseCore\Editor\Animation\SplitAnimationClipConfig.asset";
+
+        public List<string> sourceFolders = new List<string>();
+
+        public string targetTopFolder;
+
+        public bool CheckFolderPath()
+        {
+            bool isCanSplit = targetTopFolder.IndexOf("Assets/") != -1 && !string.IsNullOrEmpty(targetTopFolder) && Directory.Exists(Path.GetFullPath(targetTopFolder));
+
+            sourceFolders.RemoveAll(item =>
+            {
+                return item.IndexOf("Assets/") == -1 || string.IsNullOrEmpty(item) || !Directory.Exists(Path.GetFullPath(item));
+            });
+
+            return isCanSplit && sourceFolders.Count > 0;
+        }
+
+        public static SplitAnimationClipConfig Load()
+        {
+            SplitAnimationClipConfig config = UnityEditor.AssetDatabase.LoadAssetAtPath<SplitAnimationClipConfig>(PATH);
+            config.CheckFolderPath();
+            return config;
+        }
+    }
+}
--- Editor/Animation/SplitAnimationClipEditor.cs
+++ Editor/Animation/SplitAnimationClipEditor.cs
@@ -9,44 +9,130 @@ using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
-public class SplitAnimationClipEditor : EditorWindow
+namespace BMBaseCore
 {
-    public List<string> paths = new List<string>();
-
-
-    private void OnGUI()
+    public class SplitAnimationClipEditor : EditorWindow
     {
-        for (int i = 0; i < paths.Count; i++)
+        private float windowWith9;
+
+        private SplitAnimationClipConfig _config;
+
+        private void OnEnable()
         {
-            GUILayout.Label(paths[i]);
+            _config = SplitAnimationClipConfig.Load();
+            windowWith9 = position.width / 9;
         }
-        if (GUILayout.Button("Add"))
+
+        private void OnDestroy()
         {
-            string folderPath = EditorUtility.OpenFolderPanel("select folder", "", "");
-
-            int index = folderPath.IndexOf("Assets/");
-
-            if (index == -1) { return; }
-
-            paths.Add(folderPath.Substring(index));
-            
-            //_buildAbConfig.folderPaths[i] = folderPath.Substring(index);
+            _config.CheckFolderPath();
         }
+
+        #region GUI
+
+        private void OnGUI()
+        {
+            windowWith9 = position.width / 9;
+
+            DrawTargetPath();
+            DrawSourcePaths();
+
+            if (GUILayout.Button("Split"))
+            {
+                Split();
+            }
+        }
+
+        private void DrawSourcePaths()
+        {
+            GUILayout.Space(10);
+
+            for (int i = 0; i < _config.sourceFolders.Count; i++)
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label($"Source Path {i + 1}", GUILayout.Width(windowWith9));
+                    GUILayout.Box(_config.sourceFolders[i], GUILayout.Width(5.5f * windowWith9));
+                    if (GUILayout.Button("...", GUILayout.Width(windowWith9)))
+                    {
+                        string folderPath = EditorUtility.OpenFolderPanel("select folder", "", "");
+
+                        int index = folderPath.IndexOf("Assets/");
+
+                        if (index == -1) { return; }
+
+                        _config.sourceFolders[i] = folderPath.Substring(index);
+                    }
+                    if (GUILayout.Button("del", GUILayout.Width(windowWith9)))
+                    {
+                        _config.sourceFolders.RemoveAt(i);
+                    }
+                }
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.Space(20);
+
+
+            if (GUILayout.Button("Add Source Path"))
+            {
+                _config.sourceFolders.Add(string.Empty);
+            }
+
+            GUILayout.Space(10);
+        }
+
+        private void DrawTargetPath()
+        {
+            GUILayout.Space(10);
+
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label($"Target Top Path", GUILayout.Width(windowWith9));
+                GUILayout.Box(_config.targetTopFolder, GUILayout.Width(5.5f * windowWith9));
+                if (GUILayout.Button("...", GUILayout.Width(windowWith9 * 2)))
+                {
+                    string folderPath = EditorUtility.OpenFolderPanel("select folder", "", "");
+
+                    int index = folderPath.IndexOf("Assets/");
+
+                    if (index == -1) { return; }
+
+                    _config.targetTopFolder = folderPath.Substring(index);
+                }
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+        }
+
+        #endregion
+
+
+        private void Split()
+        {
+
+            if (!_config.CheckFolderPath())
+            {
+                EditorUtility.DisplayDialog("Error", "Config is not right","got it");
+                return;
+            }
+
+
+        }
+
+        private AnimationClip CopyAnimationClip(AnimationClip sourceClip)
+        {
+            AnimationClip newClip = new AnimationClip();
+            var editorCurves = AnimationUtility.GetCurveBindings(sourceClip);
+
+            for (int i = 0; i < editorCurves.Length; i++)
+            {
+                AnimationUtility.SetEditorCurve(newClip, editorCurves[i], AnimationUtility.GetEditorCurve(sourceClip, editorCurves[i]));
+            }
+
+            return newClip;
+        }
+
     }
-
-
-    private AnimationClip CopyAnimationClip(AnimationClip sourceClip)
-    {
-        AnimationClip newClip = new AnimationClip();
-        var editorCurves = AnimationUtility.GetCurveBindings(sourceClip);
-
-        for (int i = 0; i < editorCurves.Length; i++)
-        {
-            AnimationUtility.SetEditorCurve(newClip, editorCurves[i], AnimationUtility.GetEditorCurve(sourceClip, editorCurves[i]));
-        }
-
-        return newClip;
-    }
-
 }
