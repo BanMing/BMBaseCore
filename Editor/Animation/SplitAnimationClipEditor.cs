98565be224156bfcb350ee5f88e2f263f8d580f7
BanMing <ban-ming@foxmail.com>
2020-11-25 08:55:19 +0800

    add

--- Editor/Animation/SplitAnimationClipEditor.cs
+++ Editor/Animation/SplitAnimationClipEditor.cs
@@ -121,17 +121,67 @@ namespace BMBaseCore
 
             string[] files = Directory.GetFiles(Path.GetFullPath(_config.sourceFolders[0]));
 
-            Debug.Log(files[0]);
 
-            AnimationClip animationClip = AssetDatabase.LoadAssetAtPath<AnimationClip>(files[0]);
-            Debug.Log(animationClip);
-
-            var objs = AssetDatabase.LoadAllAssetsAtPath(files[0]);
-            for (int i = 0; i < objs.Length; i++)
+            for (int i = 0; i < files.Length; i++)
             {
-                Debug.Log(objs[i]);
+                if (files[i].EndsWith(".meta"))
+                {
+                    continue;
+                }
+
+                string path = files[i].Substring(files[i].IndexOf(@"Assets\"));
+                Debug.Log(path);
+
+                AnimationClip animationClip = AssetDatabase.LoadAssetAtPath<AnimationClip>(path);
+                AnimationClip singleClip = CopyAnimationClip(animationClip);
+                AssetDatabase.CreateAsset(singleClip, _config.targetTopFolder + "/" + animationClip.name + ".anim");
             }
 
+        }
+
+        static private void CompressAnim()
+        {
+            UnityEngine.Object folder = Selection.objects[0];
+            if (folder == null || folder.GetType() != typeof(DefaultAsset)) { return; }
+
+            string folderPath = AssetDatabase.GetAssetPath(folder.GetInstanceID());
+            // UnityEngine.Debug.Log(folderPath);
+            folderPath = Path.GetFullPath(folderPath);
+            string[] animPaths = Directory.GetFiles(folderPath, "*.anim");
+            for (int j = 0; j < animPaths.Length; j++)
+            {
+                string animPath = animPaths[j].Substring(animPaths[j].IndexOf("Assets\\"));
+                AnimationClip theAnimation = AssetDatabase.LoadAssetAtPath<AnimationClip>(animPath);
+                // AnimationClip theAnimation = Selection.objects[0] as AnimationClip;
+                if (theAnimation == null) return;
+
+                //浮点数精度压缩到f3
+                AnimationClipCurveData[] curves = null;
+                curves = AnimationUtility.GetAllCurves(theAnimation);
+                Keyframe key;
+                Keyframe[] keyFrames;
+                for (int ii = 0; ii < curves.Length; ++ii)
+                {
+                    AnimationClipCurveData curveDate = curves[ii];
+                    if (curveDate.curve == null || curveDate.curve.keys == null)
+                    {
+                        continue;
+                    }
+                    keyFrames = curveDate.curve.keys;
+                    for (int i = 0; i < keyFrames.Length; i++)
+                    {
+                        key = keyFrames[i];
+                        key.value = float.Parse(key.value.ToString("f3"));
+                        key.inTangent = float.Parse(key.inTangent.ToString("f3"));
+                        key.outTangent = float.Parse(key.outTangent.ToString("f3"));
+                        keyFrames[i] = key;
+                    }
+                    curveDate.curve.keys = keyFrames;
+                    theAnimation.SetCurve(curveDate.path, curveDate.type, curveDate.propertyName, curveDate.curve);
+                }
+            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
 
         private AnimationClip CopyAnimationClip(AnimationClip sourceClip)
